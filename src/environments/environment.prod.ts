968b39ce181b821707be4fc4e52bb6f47ac221d6
armansiddiqui00 <aramnsidiqui0000@gmail.com>
2025-05-22 12:12:24 +0530

    Clean commit without cached .angular files

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: true,
-//    apiUrl: 'http://localhost:4000'
-//    apiUrl : 'http://testscheckout.com'
- 
-//apiUrl : 'http://72.182.209.93:9098'
-apiUrl : 'http://localhost:8072/api/'
-//apiUrl : 'https://vendors.rootwareglobal.com/dwcsrv/api/'
-};
